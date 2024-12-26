63723a5999836297be1c8f81a31133fc16c36a3c
Maryam Alraddadi <malraddadi@elm.sa>
2024-12-26 18:42:44 +0300

    another impl for mmult blocking

--- src/mmult/impl/opt.c
+++ src/mmult/impl/opt.c
@@ -30,41 +30,31 @@ void* impl_scalar_opt(void* args)
 
   args_t* parsed_args = (args_t*)args;   
 
-  register float*       dest = (float*)(parsed_args->output);
-  register const float* src1 = (const float*)(parsed_args->input1);
-  register const float* src2 = (const float*)(parsed_args->input2);
+  register const float *src0 = (const float *)(parsed_args->input1);
+  register const float *src1 = (const float *)(parsed_args->input2);
+  register float *dest = (float *)(parsed_args->output);
 
-  register size_t       m    = parsed_args->m;
-  register size_t       p    = parsed_args->p;
-  register size_t       n    = parsed_args->n;
-  register size_t 	    b    = parsed_args->b;
-  register float 	      val;
+  register int b = parsed_args->b;
+  register int m = parsed_args->m;
+  register int n = parsed_args->n;
+  register int p = parsed_args->p;
 
 
-    for (int i = 0; i < m; i += b) {
-      for (int j = 0; j < n; j += b) {
-          for (int x = i; x < i + b && x < m; x++) {
-              for (int y = j; y < j + b && y < n; y++) {
-                  dest[x * n + y] = 0.0f;
+  float val = 0.0;
+  for (register int ii = 0; ii < m; ii += b) {
+      for (register int jj = 0; jj < p; jj += b) {
+          for (register int kk = 0; kk < n; kk += b) {
+              for (register int i = ii; i < ii + b && i < m; i++) {
+                  for (register int j = jj; j < jj + b && j < p; j++) {
+                      val = dest[i * p + j];
+                      for (register int k = kk; k < kk + b && k < n; k++) {
+                          val += src0[i * n + k] * src1[k * p + j];
+                      }
+                      dest[i * p + j] = val;
+                  }
               }
           }
       }
-  }
-  
-  for (int ii = 0; ii < m; ii += b) {
-    for (int jj = 0; jj < n; jj += b) {
-      for (int kk = 0; kk < p; kk += b) {
-        for (int i = ii; i < min(ii + b, m); i++) {
-            for (int j = jj; j < min(jj + b, n); j++) {
-                val = dest[i * n + j];
-                for (int k = kk; k < min(kk + b, p); k++) {
-                    val += src1[i * p + k] * src2[k * n + j];
-                }
-                dest[i * n + j] = val;
-          }
-        }
-      }
-    }
   }
   
 
